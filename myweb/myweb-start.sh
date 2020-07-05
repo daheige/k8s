cafb141ca15458238c56a720dceba26f418e32f0
daheige <zhuwei313@hotmail.com>
2020-07-05 12:48:26 +0800

    myweb k8s entry

---
+++ myweb/myweb-start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+root_dir=$(cd "$(dirname "$0")"; cd ..; pwd)
+
+kubectl apply -f $root_dir/recommended.yaml
+
+cd $root_dir/myweb
+
+kubectl create -f mysql-rc.yaml
+# replicationcontroller/mysql created
+
+kubectl create -f mysql-svc.yaml
+# service/mysql created
+
+kubectl create -f myweb-rc.yaml
+# replicationcontroller/myweb created
+
+kubectl create -f myweb-svc.yaml
+# service/myweb created
